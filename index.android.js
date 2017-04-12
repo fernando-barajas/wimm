e53c90cafd4c42f7a838d4ad6a792520d3adaa90
Fernando Barajas <temok7@gmail.com>
2017-04-11 23:21:21 -0500

    Add PaymentsList component

--- index.android.js
+++ index.android.js
@@ -5,49 +5,23 @@
  */
 
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry, Navigator } from 'react-native';
+import { Scene, Router } from 'react-native-router-flux';
+import PaymentsList from './components/PaymentsList';
+
+var styles = require('./styles/styles');
 
 export default class wimm extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      <Router>
+        <Scene key='root' navigationBarStyle={styles.navigationBar}>
+          <Scene key='PaymentsList' component={PaymentsList} title='WIMM' initial={true} sceneStyle={styles.scene} />
+        </Scene>
+      </Router>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('wimm', () => wimm);
